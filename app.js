26c7898d625cb1a1938529fedd5982bf206c3290
vincentotsai <totsai@139.com>
2017-04-12 14:19:43 +0800

    frame

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+
+app.set('views','./views/pages')
+app.set('view engine','pug')
+app.listen(port)
+
+console.log('Imooc is running on localhost:'+port)
+
+// index page
+app.get('/',function(req,res){
+    res.render('index',{title:'homepage',message:'welcome imooc!'})
+})
+
+// detail page
+app.get('/detail',function(req,res){
+    res.render('detail',{title:'detail page'})
+})
+
+// admin page
+app.get('/admin',function(req,res){
+    res.render('admin',{title:'admin'})
+})
+
+// list page
+app.get('/admin/list',function(req,res){
+    res.render('list',{title:' list page'})
+})
